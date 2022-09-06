a0c021a483ed04a714e8bd184f6c72ccc3e29803
HavierDai <huaizhi.dai@nre.tas.gov.au>
2022-09-07 09:27:15 +1000

    1

--- src/tech/havier/yingjieduck/DataBase/ConfigHavi1.java
+++ src/tech/havier/yingjieduck/DataBase/ConfigHavi1.java
@@ -1,7 +1,7 @@
 package tech.havier.yingjieduck.DataBase;
 
 public class ConfigHavi1 {
-    public static String user = "2";
-    public static String pwd = "1232312321321321323";
+    public static String user = "2232323232323";
+    public static String pwd = "1232312321321323232131321323";
 
 }
