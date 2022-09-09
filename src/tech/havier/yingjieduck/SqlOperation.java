c1e30cae464e4eac6f3b32960576ceed84288b23
HavierDai <huaizhi.dai@nre.tas.gov.au>
2022-09-09 14:53:48 +1000

    1

---
+++ src/tech/havier/yingjieduck/SqlOperation.java
@@ -0,0 +1,89 @@
+package tech.havier.yingjieduck;
+
+import tech.havier.yingjieduck.DataBase.ConfigHavi2;
+
+import java.sql.*;
+import java.util.Arrays;
+
+public class SqlOperation {
+
+
+    static ConfigHavi2 config = new ConfigHavi2();
+
+    public static int[][] getAllInputsFromDB() throws Exception {
+        try {
+        Connection connection = getConnection();
+        int rowCount = getRowCount(connection);
+        int[][] returnedResult = new int[rowCount][];
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM SALES_DATA order by id");
+
+            ResultSet allInputResultSet = resultSet;
+
+            int columnCount = allInputResultSet.getMetaData().getColumnCount();
+            int nestingIndex = 0;
+            while (allInputResultSet.next()) {
+                int[] oneRow = new int[columnCount - 2];
+                for(int i=2; i<=columnCount-1; i++){
+                    oneRow[i - 2] = allInputResultSet.getInt(i);
+                }
+                returnedResult[nestingIndex] = oneRow;
+                nestingIndex++;
+            }
+            return returnedResult;
+        } catch (Exception e) {
+            throw new Exception("Something wrong: " + e);
+        }
+    }
+
+    public static double[][] getAllOutputsFromDB() throws SQLException {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XEPDB1", config.a, config.b );
+
+            int rowCount = getRowCount(connection);
+            double[][] returnedResult = new double[rowCount][];
+
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT RESULT FROM SALES_DATA order by id");
+
+            int nestingIndex = 0;
+            while (resultSet.next()) {
+                int oneResult = resultSet.getInt(1);
+                String binaryStringCode = DataFormatting.generalOutputBinaryCode(oneResult);
+                double[] oneResultDouble = DataFormatting.changeBinaryStringIntoArray(binaryStringCode);
+                returnedResult[nestingIndex] = oneResultDouble;
+                nestingIndex++;
+            }
+            System.out.println("nested result set collection is: " + Arrays.deepToString(returnedResult));
+            connection.close();
+            return  returnedResult;
+        }catch (Exception e){
+            throw new SQLException("something wrong: " + e);
+        }
+    }
+
+    private static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection connection = DriverManager.getConnection(
+                "jdbc:oracle:thin:@localhost:1521/XEPDB1",
+                config.a, config.b );
+        return connection;
+    }
+
+
+    private static int getRowCount(Connection connection) throws SQLException {
+        Statement statement = connection.createStatement();
+        int rowCount = -1;
+        ResultSet rowCountSet = statement.executeQuery("SELECT COUNT(*) FROM SALES_DATA");
+        while (rowCountSet.next()) {
+            rowCount = rowCountSet.getInt(1);
+        }
+        if (rowCount < 0) {
+            throw new SQLException("no data got from database");
+        }
+        return rowCount;
+    }
+}
---
+++ src/tech/havier/yingjieduck/TrainingInputExporter.java
@@ -0,0 +1,58 @@
+package tech.havier.yingjieduck;
+
+import java.util.Arrays;
+
+/**
+ * generate 2-dimensional double array for training the neural network.
+ */
+public class TrainingInputExporter implements DataSet.Exporter {
+
+    private int[][] decimalSet;
+    private double[] trainingInputOneRow;
+    private static final int[] format = DataSet.getFormat10to2();
+
+    public static void main(String[] args) throws Exception {
+        int[][] sql = SqlOperation.getAllInputsFromDB();
+        DataSet me = new DataSet(new SqlImporter(sql));
+
+        DataSet.Exporter exporter = new TrainingInputExporter();
+        me.exportTo(exporter);
+        exporter.
+    }
+
+    @Override
+    public void saveDecimalSet(int[][] decimalSet) {
+        this.decimalSet = decimalSet;
+    }
+
+    public static double[][] toDoubleNestedArrays(){
+        System.out.println(Arrays.toString(decimalSet[1]));
+        return null;
+    }
+
+
+    public static String generateWholeBinaryCode(int[] inputArray){
+        StringBuilder binaryCode = new StringBuilder();
+        for(int i = 0; i< inputArray.length; i++){
+            int di = format[i];
+            int decimalValue = inputArray[i];
+            String format = "%0" + di + "d";
+            String rawBinaryValue = Integer.toBinaryString(decimalValue);
+            String binaryValue = String.format(format, Integer.parseInt(rawBinaryValue));
+            binaryCode.append(binaryValue);
+        }
+        return binaryCode.toString();
+    }
+
+    public static double[] changeBinaryStringIntoArray(String binaryString) {
+        String[] charArray = binaryString.split("(?!^)");
+        double[] doubleArray = new double[charArray.length];
+        for (int i = 0; i < charArray.length; i++){
+            doubleArray[i] = Double.parseDouble(charArray[i]);
+        }
+        return doubleArray;
+
+    }
+
+
+}
